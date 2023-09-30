ba8222a9b319dc769ce49cf767a610e27666ab4e
Rohan <rohandute1@gmail.com>
2023-09-30 21:23:56 +0530

    [rohan]find the maximum of three integers using CompareTo method

---
+++ Day_5_GenericsProblems/FindMaxValue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_5_GenericsProblems
+{
+    class FindMaxValue
+    {
+        public static void FindMaximumValue(int a, int b, int c)
+        {
+            if (a.CompareTo(b) >= 0 && a.CompareTo(c) >= 0)
+            {
+                Console.WriteLine($"Maximum integer is:{a}");
+            }
+            else if (b.CompareTo(a) >= 0 && b.CompareTo(c) >= 0)
+            {
+                Console.WriteLine($"Maximum integer is:{b}");
+            }
+            else if (c.CompareTo(b) >= 0 && c.CompareTo(a) >= 0)
+            {
+                Console.WriteLine($"Maximum integer is:{c}");
+            }
+        }
+    }
+}
--- Day_5_GenericsProblems/Program.cs
+++ Day_5_GenericsProblems/Program.cs
@@ -11,6 +11,30 @@ namespace Day_5_GenericsProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Generics Practice Problems.");
+            bool continueExecution = true;
+            while(continueExecution)
+            {
+                Console.WriteLine("Please choose program to perform:");
+                Console.WriteLine("1.Find maximum integer");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch(option)
+                {
+                    case 1:
+                        Console.WriteLine("Please enter 3 integers.");
+                        int x = Convert.ToInt32(Console.ReadLine());
+                        int y = Convert.ToInt32(Console.ReadLine());
+                        int z = Convert.ToInt32(Console.ReadLine());
+                        FindMaxValue.FindMaximumValue(x, y, z);
+                        break;
+                }
+                Console.WriteLine("Do you want to continue.(yes/no)");
+                string userInput = Console.ReadLine();  
+                if (userInput != "yes")
+                {
+                    continueExecution = false;
+                }
+            }
 
             Console.ReadLine();
         }
