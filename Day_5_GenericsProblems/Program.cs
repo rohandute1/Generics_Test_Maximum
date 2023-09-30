bbbfb10201199ea0a3e89a945afe1941134b04e1
Rohan <rohandute1@gmail.com>
2023-09-30 20:24:59 +0530

    [rohan]successfully added initial welcome message

---
+++ Day_5_GenericsProblems/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_5_GenericsProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generics Practice Problems.");
+
+            Console.ReadLine();
+        }
+    }
+}
